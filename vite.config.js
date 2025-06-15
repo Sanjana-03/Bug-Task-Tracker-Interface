4a0eb4fcce9a177f28035919e249fed561010dc7
Sanjana Raghuwanshi <raghuneha2803@gmail.com>
2025-06-15 19:30:38 +0530

    deploy details updated

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/bug-task-tracker/'
+  base: '/Bug-Task-Tracker-Interface/'
 });
